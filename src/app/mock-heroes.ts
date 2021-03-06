344dd7b299071370d09d05415a9c6c36218b7105
jccmuench <j.c.c.muench@gmail.com>
2021-03-06 15:42:49 +0100

    hero service

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: 'Mario'},
+    {id: 2, name: 'Donkey Kong'},
+    {id: 3, name: 'Link'},
+    {id: 4, name: 'Samus'},
+    {id: 5, name: 'Yoshi'},
+    {id: 6, name: 'Kirby'},
+    {id: 7, name: 'Fox'},
+    {id: 8, name: 'Pikachu'},
+    {id: 9, name: 'Luigi'},
+    {id: 10, name: 'Ness'},
+    {id: 11, name: 'Captain Falcon'},
+    {id: 12, name: 'Jigglypuff'}
+];
